ac861b3ab5541be5fcbfba4d0c6b6893a41c1470
larbadge <112470475+larbadge@users.noreply.github.com>
2023-04-08 15:25:20 +0300

    Added jwt util and filter for SecurityConfig. Token adds to cookies, configure cacheable for static resources

--- src/main/java/com/mikhalov/taskonaut/service/UserService.java
+++ src/main/java/com/mikhalov/taskonaut/service/UserService.java
@@ -5,6 +5,7 @@ import com.mikhalov.taskonaut.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +14,20 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public void createUser(User user) {
+        String encoded = String.format("{bcrypt}%s", new BCryptPasswordEncoder().encode(user.getPassword()));
+        user.setPassword(encoded);
+        userRepository.save(user);
+    }
+
     public User getCurrentUser() {
         String email = getCurrentUserUsername();
         return getUserByEmail(email);
     }
 
-    public User getUserByEmail(String email) {
+    public User getUserByEmail(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
     }
 
     public String getCurrentUserUsername() {
@@ -30,21 +37,4 @@ public class UserService {
                 .getName();
     }
 
-//    @Bean
-//    public CommandLineRunner addUsers() {
-//        return args -> {
-//            User user1 = new User();
-//            user1.setEmail("user@example.com");
-//            user1.setPassword(new BCryptPasswordEncoder().encode("password"));
-//            user1.setRole(UserRole.USER);
-//
-//            User user2 = new User();
-//            user2.setEmail("admin@example.com");
-//            user2.setPassword(new BCryptPasswordEncoder().encode("password"));
-//            user2.setRole(UserRole.ADMIN);
-//
-//            userRepository.save(user1);
-//            userRepository.save(user2);
-//        };
-//    }
 }
